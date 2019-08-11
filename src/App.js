106c1b569db16cf1b011a8af49a180390f7d2319
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-08-11 11:54:26 -0600

    added and positioned basic fab

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FormFab from './components/FormFab';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        <p>
+          Click on the button to open the form.
+        </p>
+        <FormFab />
 
       </header>
+      <div>
+      </div>
+
     </div>
   );
 }
